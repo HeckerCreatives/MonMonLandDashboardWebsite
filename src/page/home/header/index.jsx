d43a11521d4c66ecb5abe5b232883e6b31a795d7
Jimmy Refugio <jimmyrefugio11@gmail.com>
2023-10-20 20:43:41 +0800

    posh

--- src/page/home/header/index.jsx
+++ src/page/home/header/index.jsx
@@ -213,7 +213,7 @@ const Header = () => {
                 <MDBRow> 
                     <MDBCol className="my-3 text-center">
                     <img src={donwloadnow} className="img-fluid zoom-playnow" alt="" onClick={() => {
-                        window.location.href =  `${process.env.REACT_APP_API_URL}uploads/1697802735594.apk`
+                        window.location.href =  `${process.env.REACT_APP_API_URL}uploads/Monmonland_v1.0.0.apk`
                     }}/>                       
                     </MDBCol>
                 </MDBRow>
