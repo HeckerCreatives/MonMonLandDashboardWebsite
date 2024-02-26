56b1fe7bf87bbc5786417c1917b3a10ea41f4ed7
Jimmy Refugio <jimmyrefugio11@gmail.com>
2024-02-26 22:51:03 +0800

    posh et

--- config-overrides.js
+++ config-overrides.js
@@ -52,19 +52,19 @@ module.exports = function override(config) {
         //     // Add files you want to exclude from obfuscation
         //     []
         // ),
-        // new UglifyJsPlugin({
-        //     uglifyOptions: {
-        //       warnings: false,
-        //       parse: {},
-        //       compress: {},
-        //       mangle: true, // Note mangle.properties is false by default.
-        //       output: null,
-        //       toplevel: false,
-        //       nameCache: null,
-        //       ie8: false,
-        //       keep_fnames: false,
-        //     },
-        // }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+              warnings: false,
+              parse: {},
+              compress: {},
+              mangle: true, // Note mangle.properties is false by default.
+              output: null,
+              toplevel: false,
+              nameCache: null,
+              ie8: false,
+              keep_fnames: false,
+            },
+        }),
         
           
     );
